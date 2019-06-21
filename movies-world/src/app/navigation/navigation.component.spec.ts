37f857b4e45fdfca0675fdabd85bef8b5bda9ece
Moniv9 <mohitverma3222@gmail.com>
2019-06-21 14:02:27 +0530

    adding navigation panel

--- movies-world/src/app/navigation/navigation.component.spec.ts
+++ movies-world/src/app/navigation/navigation.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationComponent } from './navigation.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
+  let debugElement: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [NavigationComponent]
+      declarations: [NavigationComponent],
+      imports: [RouterTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
       .compileComponents();
   }));
@@ -16,10 +22,16 @@ describe('NavigationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 3 navigation links', () => {
+    const links = debugElement.queryAll(By.css('a'));
+    expect(links.length).toBe(3);
   });
 });
--- movies-world/src/app/navigation/navigation.component.ts
+++ movies-world/src/app/navigation/navigation.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  //TODO: pass links as input
   constructor() { }
 
   ngOnInit() {
   }
-
 }
